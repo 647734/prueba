a13eb6b6836f6c91dbc299febbe83acd4105f715
Francisco Javier Sevilla <FSevilla@host.docker.internal>
2021-02-25 20:19:35 +0100

    Segunda Revisión.

--- HolaMundo/src/prueba/Main.java
+++ HolaMundo/src/prueba/Main.java
@@ -3,9 +3,9 @@ package prueba;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hola Mundo");
-
+		System.out.println("Esto es un proyecto de prueba");
+		
 	}
-
 }
