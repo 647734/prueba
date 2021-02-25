c756174108921108ce95decac906d183abbf6afa
Francisco Javier Sevilla <FSevilla@host.docker.internal>
2021-02-25 20:37:09 +0100

    Tercera Revisión.

--- HolaMundo/src/prueba/Main.java
+++ HolaMundo/src/prueba/Main.java
@@ -1,11 +1,17 @@
 package prueba;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hola Mundo");
 		System.out.println("Esto es un proyecto de prueba");
-		
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		Date date = new Date();
+		System.out.println("Hora actual: " + dateFormat.format(date));
 	}
 }
